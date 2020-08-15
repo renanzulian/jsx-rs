c167d95f799eeca27e7df3e5d30d3c2ddbf9ffb3
Renan Zulian <renanzulian@gmail.com>
2020-08-15 18:22:35 -0300

    Metodo POST

--- loja/loja/src/main/java/br/com/alura/loja/resource/ProjetoResource.java
+++ loja/loja/src/main/java/br/com/alura/loja/resource/ProjetoResource.java
@@ -1,10 +1,13 @@
 package br.com.alura.loja.resource;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.dao.ProjetoDAO;
 import br.com.alura.loja.modelo.Projeto;
@@ -18,5 +21,13 @@ public class ProjetoResource {
 	public String buscarProjetos(@PathParam("id") long id){
 		Projeto projeto = new ProjetoDAO().busca(id);
 		return projeto.toXML();
+	}
+	
+	@POST
+	@Produces(MediaType.APPLICATION_XML)
+	public String adicionarProjeto(String dados) {
+		Projeto projeto = (Projeto) new XStream().fromXML(dados);
+		new ProjetoDAO().adiciona(projeto);
+		return "<status>Sucesso</status>";
 	}
 }
--- loja/loja/src/test/java/br/com/alura/loja/ClienteTest.java
+++ loja/loja/src/test/java/br/com/alura/loja/ClienteTest.java
@@ -4,7 +4,10 @@ import static org.junit.Assert.assertEquals;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.junit.After;
@@ -14,6 +17,7 @@ import org.junit.Test;
 import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
 
 public class ClienteTest {
 	
@@ -38,5 +42,22 @@ public class ClienteTest {
 		Carrinho carrinho = (Carrinho) new XStream().fromXML(content);
 		assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
 	
+	}
+	
+	@Test
+	public void adicionaUmCarrinho() {
+		Carrinho carrinho = new Carrinho();
+		carrinho.adiciona(new Produto(314L, "Tablet", 999, 1));
+        carrinho.setRua("Rua Vergueiro");
+        carrinho.setCidade("Sao Paulo");
+        String xml = carrinho.toXML();
+        
+        Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080");
+        
+        Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
+
+        Response response = target.path("/carrinhos").request().post(entity);
+        assertEquals("<status>Sucesso</status>", response.readEntity(String.class));
 	}
 }
